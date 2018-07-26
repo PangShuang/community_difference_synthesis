525d78cc75608133a844325236a453552887e0b5
La Pierre <LaPierreK@SI.EDU>
2018-07-26 08:50:27 -0400

    Chi-squared to test for proportional differences in responses by treatment types.

---
+++ test proportional differences.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+
+
+#kim
+setwd('C:\\Users\\lapie\\Dropbox (Smithsonian)\\working groups\\converge diverge working group\\converge_diverge\\datasets\\LongForm')
+
+#without intercept-only significance
+correProp <- read.csv('proportions significant comp difference_by trt type.csv')%>%
+  filter(total_possible>4)%>%
+  mutate(no_mean_change=total_possible-mean_change_nointercept, no_richness_change=total_possible-richness_change_nointercept)
+
+###treatment type
+#richness responses
+prop.test(x=as.matrix(correProp[,c('richness_change_nointercept', 'no_richness_change')]), alternative='two.sided')
+#compositional responses
+prop.test(x=as.matrix(correProp[,c('mean_change_nointercept', 'no_mean_change')]), alternative='two.sided')
+
+
+###treatment category
+correPropCategory <- correProp%>%
+  group_by(resource_category)%>%
+  summarise(sum_richness_change_nointercept=sum(richness_change_nointercept), sum_no_richness_change=sum(no_richness_change), sum_mean_change_nointercept=sum(mean_change_nointercept), sum_no_mean_change=sum(no_mean_change))%>%
+  ungroup()
+
+#richness responses
+prop.test(x=as.matrix(correPropCategory[,c('sum_richness_change_nointercept', 'sum_no_richness_change')]), alternative='two.sided')
+#compositional responses
+prop.test(x=as.matrix(correPropCategory[,c('sum_mean_change_nointercept', 'sum_no_mean_change')]), alternative='two.sided')
