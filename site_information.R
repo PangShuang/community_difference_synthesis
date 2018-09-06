7dafc218e4cafa8e79830f942cc7f8db34477729
La Pierre <LaPierreK@SI.EDU>
2018-09-06 11:23:10 -0400

    Update to rarefy richness.

--- site_information.R
+++ site_information.R
@@ -10,7 +10,7 @@ library(tidyverse)
 library(vegan)
 
 #kim
-setwd('C:\\Users\\lapie\\Dropbox (Smithsonian)\\working groups\\converge diverge working group\\converge_diverge\\datasets\\LongForm')
+setwd('C:\\Users\\la pierrek\\Dropbox (Smithsonian)\\working groups\\converge diverge working group\\converge_diverge\\datasets\\LongForm')
 
 #meghan
 setwd("~/Dropbox/converge_diverge/datasets/LongForm")
@@ -62,7 +62,7 @@ expANPP<-merge(allANPP, expList2, by=c("site_code","project_name","community_typ
 
 
 
-###calculate chao richness and rarefied richness for each site
+###calculate rarefied richness for each site
 
 #import species abundance data
 species <- read.csv("SpeciesRawAbundance_Oct2017.csv")%>%
@@ -80,18 +80,17 @@ sampleIntensity<-species%>%
   ungroup()
 
 #generate list of projects to calculate rarefied richness for
-exp<-sampleIntensity%>%
+set<-sampleIntensity%>%
   select(exp)
 
-
 #create empty dataframe for loop
-estimatedRichness=data.frame(row.names=1) 
+rarefiedRichness=data.frame(row.names=1) 
 
-for(i in 1:length(exp$exp)) {
-
+for(i in 1:length(set$exp)) {
+  
   #creates a dataset for each unique experiment
   subset <- species%>%
-    filter(exp==exp$exp[i])%>%
+    filter(exp==set$exp[i])%>%
     select(exp, plot_id, calendar_year, genus_species, abundance)
   
   #transpose data into wide form
@@ -99,24 +98,26 @@ for(i in 1:length(exp$exp)) {
     spread(genus_species, abundance, fill=0)
   
   #calculate species accumulation curves
-  pool <- poolaccum(speciesData[,4:ncol(speciesData)], permutations=100)
-  chao <- as.data.frame(as.matrix(pool$chao))#this gives us estimated richness from 1-X samples
-  chao$aveChao<-rowMeans(chao)
-  chao$n<-row.names(chao)
-  chao$exp<-exp$exp[i]
-  chao2<-chao%>%
-    select(exp,n, aveChao)
+  pool <- specaccum(speciesData[,4:ncol(speciesData)], permutations=100, method='random')
+  estRichness <- as.data.frame(as.matrix(pool$richness))#this gives us estimated richness from 1-X samples
+  estRichness$n<-row.names(estRichness)
+  estRichness$exp<-set$exp[i]
   
   #rbind back
-  estimatedRichness<-rbind(chao2, estimatedRichness)
+  rarefiedRichness<-rbind(estRichness, rarefiedRichness)
 }
 
+#visualize species accumulations curves for each site
+ggplot(data=rarefiedRichness, aes(x=as.integer(n), y=V1, color=exp)) +
+  geom_smooth() + theme(legend.position='none') + coord_cartesian(xlim=c(0,34))
+
+
 #generate a list of estimated richness for each project
-expRichness<-estimatedRichness%>%
+rarefiedRichness34<-rarefiedRichness%>%
   filter(n==34)%>%#the lowest sampling intensity
   separate(exp, c("site_code", "project_name", "community_type"), sep="::")%>%
-  mutate(rrich=aveChao)%>%
-  select(-n, -aveChao)
+  mutate(rrich=V1)%>%
+  select(-n, -V1)
 
 #generate a list of project lengths
 expLength<-expInfo%>%
@@ -125,7 +126,7 @@ expLength<-expInfo%>%
   summarize(experiment_length=max(treatment_year))
 
 #merge estimated richness with project length and ANPP
-expDetails<-expRichness%>%
+expDetails<-rarefiedRichness34%>%
   left_join(expLength)%>%
   left_join(expANPP)
 
@@ -141,6 +142,6 @@ siteClimate<-read.csv("siteList_climate_Feb2016.csv")%>%
 siteExpDetails<-merge(siteClimate, expDetails, by="site_code")
 
 # #kim
-# write.csv(siteExpDetails, 'C:\\Users\\lapie\\Dropbox (Smithsonian)\\working groups\\converge diverge working group\\converge_diverge\\datasets\\LongForm\\SiteExperimentDetails_Dec2016.csv')
+# write.csv(siteExpDetails, 'C:\\Users\\la pierrek\\Dropbox (Smithsonian)\\working groups\\converge diverge working group\\converge_diverge\\datasets\\LongForm\\SiteExperimentDetails_09062018.csv')
 
 rm(list=setdiff(ls(), "siteExpDetails"))
