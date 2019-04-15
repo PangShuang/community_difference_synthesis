d19c1403406d02695c2158d38fbb72513917b9e2
La Pierre <LaPierreK@SI.EDU>
2019-04-15 13:30:22 -0400

    Proportional differences for long-term datasets.

--- test proportional differences.R
+++ test proportional differences.R
@@ -14,6 +14,7 @@ setwd('C:\\Users\\lapie\\Dropbox (Smithsonian)\\working groups\\CoRRE\\converge_
 #kim's desktop
 setwd('C:\\Users\\la pierrek\\Dropbox (Smithsonian)\\working groups\\CoRRE\\converge_diverge\\datasets\\LongForm')
 
+####all data---------------
 #significant lines
 correTotal <- read.csv('stdtimebytrt_shape_classification_04072019.csv')%>%
   select(variable, trt_type, site_code, project_name, community_type, treatment, N01_shape)%>%
@@ -67,6 +68,100 @@ correTotalCategory <- read.csv('stdtimebytrt_shape_classification_04072019.csv')
   ungroup()
 
 correPropCategory <- read.csv('stdtimebytrt_shape_classification_04072019.csv')%>%
+  select(variable, trt_type, site_code, project_name, community_type, treatment, N01_shape)%>%
+  mutate(trt_category=ifelse(trt_type %in% c('threeway','RxRxR','RxR','RxN','NxN'), as.character(trt_type), ifelse(trt_type %in% c('temp','herb_removal','mow_clip','plant_mani','other_nonresource'), 'single_nonresource', 'single_resource')))%>%
+  mutate(sig_line=ifelse(N01_shape==0, 0, 1))%>%
+  group_by(variable, trt_category)%>%
+  summarise(count_sig=sum(sig_line))%>%
+  ungroup()%>%
+  left_join(correTotalCategory)%>%
+  mutate(count_nonsig=total_possible-count_sig)%>%
+  mutate(proportion=count_sig/total_possible)
+
+#richness responses
+correPropCategoryRich <- correPropCategory%>%filter(variable=='richness')
+prop.test(x=as.matrix(correPropCategoryRich[,c('count_sig', 'total_possible')]), alternative='two.sided')
+#compositional responses
+correPropCategoryComp <- correPropCategory%>%filter(variable=='mean')
+prop.test(x=as.matrix(correPropCategoryComp[,c('count_sig', 'total_possible')]), alternative='two.sided')
+
+
+###treatment type -- single factor treatments only
+#richness responses
+correPropRich <- correPropCategory%>%filter(variable=='richness')%>%select(trt_category,count_sig,count_nonsig)
+trtType <- as.character(correPropRich$trt_category)
+correPropRichMatrix <- as.matrix(correPropRich[,c('count_sig', 'count_nonsig')], ncol=2, dimnames=list(rownames(c('NxN','RxN','RxR','RxRxR','single_nonresource','single_resource','threeway')), colnames(c('count_sig','count_nonsig'))), byrow=T)
+fisher.test(correPropRichMatrix, workspace = 2e8)
+fisher.multcomp(correPropRichMatrix)
+#compositional responses
+correPropComp <- correPropCategory%>%filter(variable=='mean')%>%select(trt_category,count_sig,count_nonsig)
+trtType <- as.character(correPropComp$trt_category)
+correPropCompMatrix <- as.matrix(correPropComp[,c('count_sig', 'count_nonsig')], ncol=2, dimnames=list(rownames(c('NxN','RxN','RxR','RxRxR','single_nonresource','single_resource','threeway')), colnames(c('count_sig','count_nonsig'))), byrow=T)
+fisher.test(correPropCompMatrix, workspace = 2e8)
+fisher.multcomp(correPropCompMatrix)
+
+
+
+
+#####10 year or longer-----------------
+
+#significant lines
+correTotal <- read.csv('stdtimebytrt_shape_classification_04072019.csv')%>%
+  filter(experiment_length>9)%>%
+  select(variable, trt_type, site_code, project_name, community_type, treatment, N01_shape)%>%
+  group_by(variable, trt_type)%>%
+  summarise(total_possible=length(trt_type))%>%
+  ungroup()
+
+correProp <- read.csv('stdtimebytrt_shape_classification_04072019.csv')%>%
+  filter(experiment_length>9)%>%
+  select(variable, trt_type, site_code, project_name, community_type, treatment, N01_shape)%>%
+  mutate(sig_line=ifelse(N01_shape==0, 0, 1))%>%
+  group_by(variable, trt_type)%>%
+  summarise(count_sig=sum(sig_line))%>%
+  ungroup()%>%
+  left_join(correTotal)%>%
+  mutate(count_nonsig=total_possible-count_sig)%>%
+  filter(trt_type %in% c('CO2','drought','herb_removal','irr','mow_clip','N','other_nonresource','other_resource','P','plant_mani','precip_vari','temp'))%>%
+  mutate(proportion=count_sig/total_possible)
+
+###treatment type -- single factor treatments only
+#richness responses
+correPropRich <- correProp%>%filter(variable=='richness')
+prop.test(x=as.matrix(correPropRich[,c('count_sig', 'total_possible')]), alternative='two.sided')
+#compositional responses
+correPropComp <- correProp%>%filter(variable=='mean')
+prop.test(x=as.matrix(correPropComp[,c('count_sig', 'total_possible')]), alternative='two.sided')
+
+
+###treatment type -- single factor treatments only
+#richness responses
+correPropRich <- correProp%>%filter(variable=='richness')%>%select(trt_type,count_sig,count_nonsig)
+trtType <- as.character(correPropRich$trt_type)
+correPropRichMatrix <- as.matrix(correPropRich[,c('count_sig', 'count_nonsig')], ncol=2, dimnames=list(rownames(c('CO2','drought','herb_removal','irr','mow_clip','N','other_nonresource','other_resource','P','plant_mani','precip_vari','temp')), colnames(c('count_sig','count_nonsig'))), byrow=T)
+fisher.test(correPropRichMatrix, workspace = 2e8)
+fisher.multcomp(correPropRichMatrix)
+#compositional responses
+correPropComp <- correProp%>%filter(variable=='mean')%>%select(trt_type,count_sig,count_nonsig)
+trtType <- as.character(correPropComp$trt_type)
+correPropCompMatrix <- as.matrix(correPropComp[,c('count_sig', 'count_nonsig')], ncol=2, dimnames=list(rownames(c('CO2','drought','herb_removal','irr','mow_clip','N','other_nonresource','other_resource','P','plant_mani','precip_vari','temp')), colnames(c('count_sig','count_nonsig'))), byrow=T)
+fisher.test(correPropCompMatrix, workspace = 2e8)
+fisher.multcomp(correPropCompMatrix)
+
+
+
+###treatment category
+#significant lines
+correTotalCategory <- read.csv('stdtimebytrt_shape_classification_04072019.csv')%>%
+  filter(experiment_length>9)%>%
+  select(variable, trt_type, site_code, project_name, community_type, treatment, N01_shape)%>%
+  mutate(trt_category=ifelse(trt_type %in% c('threeway','RxRxR','RxR','RxN','NxN'), as.character(trt_type), ifelse(trt_type %in% c('temp','herb_removal','mow_clip','plant_mani','other_nonresource'), 'single_nonresource', 'single_resource')))%>%
+  group_by(variable, trt_category)%>%
+  summarise(total_possible=length(trt_category))%>%
+  ungroup()
+
+correPropCategory <- read.csv('stdtimebytrt_shape_classification_04072019.csv')%>%
+  filter(experiment_length>9)%>%
   select(variable, trt_type, site_code, project_name, community_type, treatment, N01_shape)%>%
   mutate(trt_category=ifelse(trt_type %in% c('threeway','RxRxR','RxR','RxN','NxN'), as.character(trt_type), ifelse(trt_type %in% c('temp','herb_removal','mow_clip','plant_mani','other_nonresource'), 'single_nonresource', 'single_resource')))%>%
   mutate(sig_line=ifelse(N01_shape==0, 0, 1))%>%
